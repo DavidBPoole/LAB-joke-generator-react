318ed22816ff3da45661349d117c30972aee4d2a
David Poole <dbpoole7@gmail.com>
2023-07-11 19:38:09 -0500

    react joke generator mvp complete

---
+++ components/Jokes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Jokes({ joke, btnText }) {
+  return (
+    <>
+      <h1 style={{ color: 'darkblue' }}>{joke.setup}</h1>
+      <h1 style={{ color: 'blue' }}>{btnText !== 'Get Punchline' ? joke.punchline : ''}</h1>
+    </>
+  );
+}
+
+Jokes.propTypes = {
+  joke: PropTypes.shape({
+    setup: PropTypes.string,
+    punchline: PropTypes.string,
+  }).isRequired,
+  btnText: PropTypes.string.isRequired,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,54 @@
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import getJoke from '../api/jokeData';
+import Jokes from '../components/Jokes';
+
 function Home() {
+  const [joke, setJoke] = useState({});
+  const [btnText, setBtnText] = useState('Get A Joke');
+
+  const handleClick = (text) => {
+    setBtnText(text);
+  };
+
+  const getAJoke = () => {
+    getJoke().then((obj) => {
+      setJoke({
+        setup: obj.setup,
+        punchline: obj.delivery,
+      });
+    });
+    handleClick('Get Punchline');
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home!</h1>
-    </div>
+    <>
+      <Jokes joke={joke} btnText={btnText} />
+      {btnText === 'Get A Joke' || btnText === 'Get A New Joke' ? (
+        <Button type="button" onClick={getAJoke}>
+          {btnText}
+        </Button>
+      ) : (
+        <Button type="button" onClick={() => handleClick('Get A New Joke')}>
+          {btnText}
+        </Button>
+      )}
+    </>
   );
 }
 
 export default Home;
+
+// ORIGINAL COE ABOVE FROM TEMPLATE
+// <div
+// className="text-center d-flex flex-column justify-content-center align-content-center"
+// style={{
+//   height: '90vh',
+//   padding: '30px',
+//   maxWidth: '400px',
+//   margin: '0 auto',
+// }}
+// >
+// <h1>Joke Generator</h1>
+// <button type="button" onClick={handleClick}>Get A Joke</button>
+// </div>
